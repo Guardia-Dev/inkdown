7c92390e8486cea850e886a12bcf0ec0c8832097
Desgard_Duan <gua@desgard.com>
2017-08-27 07:43:52 +0800

    Initial proj
    Signed-off-by: Desgard_Duan <gua@desgard.com>

---
+++ inkdown/inkdown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  inkdown
+//
+//  Created by 段昊宇 on 2017/8/27.
+//  Copyright © 2017年 Desgard_Duan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+            
+        }
+    }
+
+
+}
+
